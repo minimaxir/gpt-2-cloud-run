bc949a0ad149005c5f5a4b46f666764b695fbcf2
Max Woolf <max@minimaxir.com>
2019-05-29 18:41:28 -0700

    revert Ray support

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ async def homepage(request):
                          length=int(params.get('length', 1023)),
                          temperature=float(params.get('temperature', 0.7)),
                          top_k=int(params.get('top_k', 0)),
+                         top_p=float(params.get('top_p', 0.0)),
                          prefix=params.get('prefix', None),
                          truncate=params.get('truncate', None),
                          include_prefix=str(params.get(
@@ -29,6 +30,7 @@ async def homepage(request):
                          )[0]
 
     return UJSONResponse({'text': text})
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
