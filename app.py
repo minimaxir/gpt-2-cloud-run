60e9681263b4d7eea646d0e291967c2ca51e59e8
Max Woolf <max@minimaxir.com>
2019-06-08 20:37:05 -0700

    CORS + HEAD support

--- app.py
+++ app.py
@@ -9,18 +9,27 @@ app = Starlette(debug=False)
 sess = gpt2.start_tf_sess(threads=1)
 gpt2.load_gpt2(sess)
 
+# Needed to avoid cross-domain issues
+response_header = {
+    'Access-Control-Allow-Origin': '*'
+}
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/', methods=['GET', 'POST', 'HEAD'])
 async def homepage(request):
     if request.method == 'GET':
         params = request.query_params
     elif request.method == 'POST':
         params = await request.json()
+    elif request.method == 'HEAD':
+        return UJSONResponse({'text': ''},
+                             headers=response_header)
 
     text = gpt2.generate(sess,
                          length=int(params.get('length', 1023)),
                          temperature=float(params.get('temperature', 0.7)),
                          top_k=int(params.get('top_k', 0)),
+                         top_p=float(params.get('top_p', 0)),
                          prefix=params.get('prefix', None),
                          truncate=params.get('truncate', None),
                          include_prefix=str(params.get(
@@ -28,7 +37,8 @@ async def homepage(request):
                          return_as_list=True
                          )[0]
 
-    return UJSONResponse({'text': text})
+    return UJSONResponse({'text': text},
+                         headers=response_header)
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
